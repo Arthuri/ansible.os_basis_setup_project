5a66887d41bead7591af126d1b737483f1c4b1ea
Chris Ruettimann <chris@bitbull.ch>
2019-05-02 12:27:07 +0200

    qa

--- install_0_prep_ansible.sh
+++ install_0_prep_ansible.sh
@@ -6,11 +6,11 @@
 set -e
 PDIR=/etc/ansible/projects-dev
 rpm -q epel-release >/dev/null 2>&1 || yum -y install epel-release
-rpm -q ansible >/dev/null 2>&1 || yum -y install ansible
+rpm -q ansible >/dev/null 2>&1 || yum -y install ansible git
 mkdir $PDIR
 cd $PDIR
 git clone https://github.com/uniQconsulting-ag/ansible.os_basis_setup_project.git
 cd $PDIR/ansible.os_basis_setup_project
 test -d roles && rm -rf roles
-ansible-galaxy install uniqconsulting.open_vm_tools uniQconsulting.os_basic
+ansible-galaxy install uniqconsulting.open_vm_tools uniqconsulting.os_basic
 echo done
--- install_9_remove_ansible.sh
+++ install_9_remove_ansible.sh
@@ -3,5 +3,5 @@
 # DATE: 20190502
 # WHO: Chris Ruettimann cruettimann@uniqconsulting.ch
 
-rpm -q ansible >/dev/null 2>&1 || yum -y remove ansible
+rpm -q ansible >/dev/null 2>&1 || yum -y remove ansible git
 echo done
