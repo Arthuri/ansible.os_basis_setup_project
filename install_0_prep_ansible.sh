ce0b02dbf1bd6fdb6d6bd2df775d4f4d9f6cd48d
Chris Rüttimann <14975048+joe-speedboat@users.noreply.github.com>
2019-06-04 20:22:24 +0200

    error handling

--- install_0_prep_ansible.sh
+++ install_0_prep_ansible.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 # DESC: setup ansible control host
 # DATE: 20190502
 # WHO: Chris Ruettimann cruettimann@uniqconsulting.ch
 
-set -e
+
 PDIR=/etc/ansible/projects
 rpm -q epel-release >/dev/null 2>&1 || yum -y install epel-release
 rpm -q git >/dev/null 2>&1 || yum -y install ansible git
 rpm -q ansible >/dev/null 2>&1 || yum -y install ansible git
-mkdir $PDIR
+mkdir $PDIR || true
 cd $PDIR
 git clone https://github.com/uniQconsulting-ag/ansible.os_basis_setup_project.git
 cd $PDIR/ansible.os_basis_setup_project
